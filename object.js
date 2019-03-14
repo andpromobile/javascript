97c756db9215943e905f6ec2a7ba37263a5ca681
andpromobile <andpromobile@gmail.com>
2019-03-14 16:03:01 +0700

    menambahkan array dalam object

--- object.js
+++ object.js
@@ -3,10 +3,13 @@ function panggilObject() {
     type:"Sedan",
     harga: 50000,
     warna: "Putih",
+    tahun: [2001, 2002, 2003, 2004]
   }
 
   console.log(mobil);
   console.log(mobil.harga);
+  console.log(mobil.tahun);
+  console.log(mobil.tahun[1]);
 }
 
 panggilObject();
