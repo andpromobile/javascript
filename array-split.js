70709ce63aa189347baac5f482e310f9f64d175a
andpromobile <andpromobile@gmail.com>
2019-03-14 17:33:10 +0700

    Menggunakan array split

---
+++ array-split.js
@@ -0,0 +1,9 @@
+function panggilSplit() {
+  var kalimat = "Kita sedang belajar Javascript di Niomic";
+console.log(kalimat);
+  var result = kalimat.split(" ");
+
+  return result;
+}
+
+console.log(panggilSplit());
