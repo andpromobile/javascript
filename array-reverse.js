3828c49edf968c5d110620b53445a50b558c3263
andpromobile <andpromobile@gmail.com>
2019-03-14 17:41:14 +0700

    Belajar menggunakan reverse pada array

---
+++ array-reverse.js
@@ -0,0 +1,10 @@
+function panggilReverse() {
+  var kota = ["Jakarta", "Bandung", "Malang", "Banjarmasin"];
+console.log(kota);
+
+var result = kota.reverse();
+
+return result;
+}
+
+console.log(panggilReverse());
