a3f6781e2a46ba2959c880fb816372dc6f4e5182
andpromobile <andpromobile@gmail.com>
2019-03-14 15:44:57 +0700

    menambahkan index, function, if-else

---
+++ index.js
@@ -0,0 +1,16 @@
+var message = "Hello Coding";
+let msg1 = "hello";
+const msg2 = "hello";
+
+message = "rubah 1";
+msg1 = "rubah 2";
+
+if (true) {
+  var message = 1;
+  let msg1 = 2;
+  console.log("testing", msg1);
+}
+
+console.log(message);
+console.log(msg1);
+console.log(msg2);
