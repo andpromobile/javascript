eaaa105910df0dfa923868e6492bf56a17026539
andpromobile <andpromobile@gmail.com>
2019-03-14 18:25:58 +0700

    Bilangan Ganjil di Javascript

---
+++ BilanganGanjil.js
@@ -0,0 +1,13 @@
+function panggilBilanganGanjil() {
+  var BilanganGanjil = [];
+
+  for (var i = 1; i < 30; i++) {
+    if (i % 2 !== 0) {
+      BilanganGanjil.push(i);
+    }
+  }
+
+  return BilanganGanjil;
+}
+
+console.log(panggilBilanganGanjil());
