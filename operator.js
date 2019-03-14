33563774065250de3e5f8c191e4babad5b2871ae
andpromobile <andpromobile@gmail.com>
2019-03-14 16:59:26 +0700

    Belajar operator

---
+++ operator.js
@@ -0,0 +1,13 @@
+function panggilOperator() {
+  var a = 4;
+  var b = 3;
+
+  // console.log(a+b);
+  // console.log(a-b);
+  // console.log(a*b);
+  // console.log(a/b);
+  return a*b;
+}
+
+// panggilOperator();
+console.log(panggilOperator());
