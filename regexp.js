02be013b2094ebf70448a35deace046e1eaed304
andpromobile <andpromobile@gmail.com>
2019-03-14 19:58:36 +0700

    Latihan menggunakan regexp

---
+++ regexp.js
@@ -0,0 +1,12 @@
+function panggilRegexp() {
+ let str = "abcdefghijklmnopqrstuvwxyz";
+
+ // return str.search(/u/)
+ console.log(str.search(/K/))
+ console.log(str.search(/K/i))
+ console.log(str.search(/k/i))
+}
+
+
+// console.log(panggilRegexp())
+panggilRegexp();
