f8dfa4dcf6243e88e19a5799d575d99b7edd41aa
andpromobile <andpromobile@gmail.com>
2019-03-14 15:58:18 +0700

    menambahkan file nested-array.js

---
+++ nested-array.js
@@ -0,0 +1,8 @@
+function panggilNestedArray() {
+  var dataNama = [["Aku", "Kamu", "Mereka"],
+                  ["Bola", "Basket", "Voli"],
+                  ["Juventus", "AC Milan", "AS Roma"]];
+  console.log(dataNama[0][0]);
+}
+
+panggilNestedArray();
