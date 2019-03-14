77d8b83bedf95f928f205af191e16dcbef746ff1
andpromobile <andpromobile@gmail.com>
2019-03-14 17:35:38 +0700

    Menggunakan joinpada array

---
+++ array-join.js
@@ -0,0 +1,9 @@
+function panggilJoin() {
+  var kota = ["Jakarta", "Bandung", "Malang", "Banjarmasin"];
+console.log(kota);
+var result = kota.join(" ");
+
+return result;
+}
+
+console.log(panggilJoin());
