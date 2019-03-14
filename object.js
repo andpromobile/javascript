7c4629d1d658cda484a53d32e87d80d40efd17c0
andpromobile <andpromobile@gmail.com>
2019-03-14 16:00:26 +0700

    menambahkan object.js

---
+++ object.js
@@ -0,0 +1,12 @@
+function panggilObject() {
+  var mobil = {
+    type:"Sedan",
+    harga: 50000,
+    warna: "Putih",
+  }
+
+  console.log(mobil);
+  console.log(mobil.harga);
+}
+
+panggilObject();
