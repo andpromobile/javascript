c7bf372f6413d01c9011e2442a8673634ce562ae
andpromobile <andpromobile@gmail.com>
2019-03-14 17:30:36 +0700

    Menggunakan Concat untuk manipulasi array

---
+++ array-concat.js
@@ -0,0 +1,11 @@
+function panggilConcat() {
+  var kota = ["Jakarta", "Bandung", "Malang", "Banjarmasin"];
+
+  var kabupaten = ["Kabupaten Banjar", "Batola", "Tabalong"];
+
+  var hasil = kota.concat(kabupaten);
+
+  return hasil;
+}
+
+console.log(panggilConcat());
