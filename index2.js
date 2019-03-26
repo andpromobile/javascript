13092abcd27f9289b26ae7ff7a4dca637ebe4205
andpromobile <andpromobile@gmail.com>
2019-03-27 02:16:00 +0700

    Javascript di browser

---
+++ index2.js
@@ -0,0 +1,10 @@
+function panggilJavascript() {
+ console.log("Menuliskan Code Javascript di Browser Secara Eksternal");
+}
+
+function panggilJS() {
+ console.log("Function ke 2");
+}
+
+panggilJavascript();
+panggilJS();
